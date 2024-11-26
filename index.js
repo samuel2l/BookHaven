af0f51075ddabdb8a196d55093c2a0a59f53794f
Samuel Adams <sama29571@gmail.com>
2024-11-26 19:39:51 +0000

    add error handling

--- index.js
+++ index.js
@@ -27,19 +27,13 @@ async function main() {
 app.get('/books',async (req,res)=>{
     const books=await Book.find({})
     print(books)
-
     res.render('books.ejs',{books})
-
 })
 
-   // name
-    // price
-    // author
-    // genre
-    // stock 
 
 app.post('/books',async (req,res)=>{
     const {name,price,genre,author,stock}=req.body
+    print(genre)
     const newBook= new Book({name,price,genre,author,stock})
     newBook.save()
     .then(
@@ -50,69 +44,144 @@ app.post('/books',async (req,res)=>{
     .catch((e)=>{print(e)})
     
     res.redirect('/books')
-
 })
 
-
-app.get('/books/:id',async (req,res)=>{
+app.get('/books/:id',async (req,res,next)=>{
+try{
     const id=req.params.id
     
-    const book=await Book.find({_id:id})
-
-    
-    
+    const book=await Book.findById(id)
 
     res.render('book_details.ejs',{book:book[0]})
 
+}catch(err){
+next(err)
+}
+
+
 })
-app.get('/books/:genre/filter', async (req, res) => {
+// app.get('/books/:genre/filter', async (req, res) => {
+//     try {
+//         const genre = req.params.genre;
+
+//         const books = await Book.find({ genre: { $in: [genre] } })
+
+//         console.log('NEW ROUTEEEE')
+//         console.log(books)
+
+//         res.render('filter_by_genre.ejs', { books, genre })
+//     } catch (err) {
+//         next(err)
+
+
+//     }
+// })
+app.get('/books/filter', async (req, res) => {
     try {
-        const genre = req.params.genre;
-
-        const books = await Book.find({ genre: genre });
-        console.log('NEW ROUTEEEE');
-        console.log(books);
-
-        res.render('filter_by_genre.ejs',{books,genre})
+        const selectedGenres = req.query.genres;
+        let query = {};
+        if (selectedGenres && selectedGenres.length > 0) {
+            query = { genre: { $in: selectedGenres } };
+        }
+        const books = await Book.find(query);
+        if (books.length === 0) {
+            return res.render('filter_by_genre.ejs', { books, genres: selectedGenres });
+        }
+        res.render('filter_by_genre.ejs', { books, genres: selectedGenres });
     } catch (error) {
         console.error('Error fetching books:', error);
         res.status(500).send('An error occurred while fetching books.');
     }
+});
+
+app.get('/books/author/:author/filter', async (req, res) => {
+    try {
+        const authorQuery = req.params.author
+
+        const books = await Book.find({ author: { $regex: authorQuery, $options: 'i' } })
+
+        if (books.length === 0) {
+            return res.render('error.ejs', { message: `No books found for author "${authorQuery}".` });
+        }
+
+        res.render('filter_by_author.ejs', { books, author: authorQuery })
+    } catch (error) {
+
+       next(error)
+    }
 })
+app.get('/books/name/:name/filter', async (req, res) => {
+    try {
+        const nameQuery = req.params.name
+
+        const books = await Book.find({ name: { $regex: nameQuery, $options: 'i' } })
+
+        if (books.length === 0) {
+            return res.render('error.ejs', { message: `No books found by "${nameQuery}".` });
+        }
+
+        res.render('filter_by_name.ejs', { books, name: nameQuery })
+    } catch (err) {
+
+next(err)
+    }})
 
 app.get('/books/:id/edit',async(req,res)=>{
-    const id=req.params.id
-    const book=await Book.find({_id:id})
+    try{
+        const id=req.params.id
+        const book=await Book.find({_id:id})
+    
+        res.render('update_book.ejs',{book:book[0],id})
+    
+    }catch(err){
+        next(err)
 
-    res.render('update_book.ejs',{book:book[0],id})
+    }
 
 })
 
 
 app.patch('/books/:id',async (req,res)=>{
-    const id=req.params.id
+    try{
+        const id=req.params.id
     
-    let book=await Book.findByIdAndUpdate(id,req.body,{runValidators:true,new:true})
-
-    print(book)
-    print('HMMMMM')
-    print(req.body)
-    res.redirect(`/books/${id}`)
-
+        let book=await Book.findByIdAndUpdate(id,req.body,{runValidators:true,new:true})
+    
+        print(book)
+        print('HMMMMM')
+        print(req.body)
+        res.redirect(`/books/${id}`)
+    
+    }catch(err){
+        next(err)
+    }
+    
 })
 
 
 app.delete('/books/:id',async (req,res)=>{
-    const id=req.params.id
+    try{
+        const id=req.params.id
     
-    let deletedBook=await Book.findByIdAndDelete(id)
-
-    res.redirect(`/books`)
+        let deletedBook=await Book.findByIdAndDelete(id)
+    
+        res.redirect(`/books`)
+    
+    }catch(err){
+next(err)
+    }
 
 })
 
 
+app.get('*',(req,res)=>{
+    res.render('error.ejs',{message:'This page does not exist'})
+})
 
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.render('error.ejs',{message})
+})
 
 
 
--- models/book.js
+++ models/book.js
@@ -14,18 +14,34 @@ const bookSchema=mongoose.Schema({
         type:String,
         required:true
     },
-    genre:{
-        type:String,
-        required:true
+    genre: {
+        type: [String], 
+        required: true,
+        validate: {
+          validator: function (v) {
+            return v.every((genre) => [
+                "Action",
+                "Comedy",
+                "Drama",
+                "Fantasy",
+                "Horror",
+                "Romance",
+                "Sci-Fi",
+                "Thriller",
+                "Animation",
+              ].includes(genre));
+          },
 
-    },
-    stock:{
+          message: (props) => `${props.value} contains invalid genres.`,
+        },
+      },    stock:{
         type: Number,
         required:true,
         min:0
 
     }
 })
+
 
 const Book=mongoose.model('Book',bookSchema)
 module.exports=Book
